39f8618526bbfe2cc47dc6b406600063a05b8d18
Alexisql <alexiskinte@gmail.com>
2026-02-07 16:40:17 -0500

    Limpieza de importaciones en `ItemLocation`.

--- app/src/main/java/co/com/alexis/weather/ui/home/component/ItemLocation.kt
+++ app/src/main/java/co/com/alexis/weather/ui/home/component/ItemLocation.kt
@@ -15,11 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import co.com.alexis.weather.R
 import co.com.alexis.weather.domain.model.Location
 import co.com.alexis.weather.ui.component.SpacerComponent
